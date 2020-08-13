1a3a5f0af04a1aa6d3b3cd903373c16485f20649
Вячеслав <potapov-vi@mail.ru>
2020-08-13 09:55:10 +0500

    Change main styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './assets/logo.svg';
+import 'antd/dist/antd.css';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-content">
+      <div className="main-row">
+        <div className="left-column">
+          col-18
+          <div className="left-column-footer">
+
+          </div>
+        </div>
+        <div className="right-column">
+          col-6
+          <div className="right-column-footer">
+
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
